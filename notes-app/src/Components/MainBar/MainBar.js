795431e7a1677e0e905a194d18085b7ddb20b2c1
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-25 15:57:10 +0530

    Designed the main-bar

--- notes-app/src/Components/MainBar/MainBar.js
+++ notes-app/src/Components/MainBar/MainBar.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./MainBar.css";
 
-function MainBar() {}
+function MainBar() {
+  return (
+    <div className="notes--mainbar">
+      <div className="notes--mainbar--input">
+        <input type="text" autoFocus />
+        <br />
+        <textarea placeholder="Write your note here..." />
+      </div>
+      <div className="notes-mainbar--input--preview">
+        <h1>TITLE</h1>
+        <p>note preview</p>
+      </div>
+    </div>
+  );
+}
 
 export default MainBar;
